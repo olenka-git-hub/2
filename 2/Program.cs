5e8c0653bdd93d215346a4a569e88ba78f93a64d
Olenka <olenka0913@gmail.com>
2022-12-15 23:57:50 +0500

    2

---
+++ 2/Program.cs
@@ -0,0 +1,16 @@
+﻿int N;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!? out N);
+
+// int n = -N;
+
+// while (n <=N)
+// {
+//     Console.Write($"{n} ");
+//     n++;
+// }
+for (int n = -N; n<=N; n++)
+{
+    Console.Write($"{n} ");
+}
+
